e5aa70d14807b34f21b5adaf2973d444c99346d0
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2025-09-30 22:45:57 +1000

    (js)(refactor) modules

---
+++ assets/partition.js
@@ -0,0 +1,130 @@
+import { YCH } from "./colour.js";
+
+
+// high contrast looping pattern
+function polyrythym(seed, big, small) {
+  return (big * seed) % small;
+}
+
+
+// pseudo-random colour
+function colour_for(depth, index) {
+  let luma = 0.8;
+  let chroma = 0.075 + polyrythym(index, 11, 3) / 10;
+  let hue = 120 + polyrythym(depth, 130, 360);
+  return new YCH(luma, chroma, hue).as_hex();
+}
+
+
+// drive / file / folder
+class DataArea {
+  constructor(label, size) {
+    [this.label, this.size] = [label, size];
+    this.children = [];
+  }
+
+  static from_array(array) {
+    let [label, size, children] = [null, null, []];
+    if (array.length == 2) {
+      [label, size] = array;
+    } else if (array.length == 3) {
+      [label, size, children] = array;
+    } else {
+      console.error("invalid array");
+    }
+    let out = new DataArea(label, size);
+    out.children = children.map((a) => DataArea.from_array(a));
+    return out;
+  }
+
+  static from_json(json) {
+    let out = new DataArea(json.label, json.size);
+    let children = [];  // optional
+    if (Object.hasOwn(json, "children")) {
+      children = json.children;
+    }
+    out.children = children.map((j) => DataArea.from_json(j));
+    return out;
+  }
+
+  get used_size() {
+    let sizes = this.children.map((c) => c.size);
+    return sizes.reduce((a, b) => a + b, 0);
+  }
+
+  get free_size() {  // unmapped by children
+    return this.size - this.used_size;
+  }
+
+  alloc(label, size) {
+    if (this.free_size >= size) {
+      this.children.push(new DataArea(label, size));
+    } else {
+      console.error(`${size} > ${this.free_size}`)
+    }
+  }
+
+  free(label) {
+    this.children = this.children.filter((c) => c.label != label);
+  }
+
+  child(label) {
+    for (const c of this.children) {
+      if (c.label == label) {
+        return c;
+      }
+    }
+  }
+
+  xpath(path) {
+    let out = this;
+    for (const label of path.split("/")) {
+      out = out.child(label);
+    }
+    return out;
+  }
+
+  // TODO: tree view
+  // TODO: on click "select" callback
+
+  // WinDirStat visualiser
+  style(depth = 0, index = 0) {
+    // grid layout
+    let axes = ["row", "column"]
+    let axis = axes[depth % 2]
+    let out = [`grid-template-${axis}s:`];
+    let sizes = this.children.map(({ size }) => size);
+    sizes.push(this.free_size)
+    for (const size of sizes) { out.push(`${size}fr`); }
+    // base colour + vingette
+    let colour = colour_for(depth, index);
+    let background = [
+      `linear-gradient(0deg, ${colour}, ${colour})`,
+      "linear-gradient(to left,   #999, transparent 25%)",
+      "linear-gradient(to right,  #999, transparent 25%)",
+      "linear-gradient(to top,    #999, transparent 25%)",
+      "linear-gradient(to bottom, #999, transparent 25%)"];
+    return out.join(" ") + ";" + ` background: ${background.join(", ")};`;
+  }
+
+  as_html_wds(depth = 0, index = 0) {
+    /* WinDirStat blocks */
+    let div = document.createElement("div");
+    // TODO: xpath id
+    div.setAttribute("class", "data-area");
+    div.setAttribute("style", this.style(depth, index));
+    let i = 1;
+    for (const c of this.children) {
+      div.appendChild(c.as_html_wds(depth + 1, i));
+      i++;
+    }
+    // extra div to fill grid, if needed
+    if (this.children.length > 0 && this.free_size > 0) {
+      div.appendChild(document.createElement("div"));
+    }
+    return div;
+  }
+}
+
+
+export { DataArea };
--- assets/script.js
+++ assets/script.js
@@ -1,131 +1,50 @@
-// TODO: global drive list (top-level DataAreas)
-// TODO: load from & save to JSON
+import { DataArea } from "./partition.js";
 
-// TODO: mirror console.log etc. to <code id="log"></code>
-// -- console.assert(cond, msg)
-// -- if not cond: log.append(msg)
+// inline definition
+/* let drive_a = new DataArea("A:/", 200);
+*  drive_a.alloc("foo", 20);
+*  drive_a.child("foo").alloc("rhubarb", 5);
+*  drive.alloc("bar", 10);
+*/
 
-// let colour = await import("./colour.js");
+// from_json
+/* let json = {
+*    label: "A:/", size: 200, children: [
+*      { label: "foo", size: 20, children: [
+*        { label: "bar", size: 10},
+*        { label: "rhubarb", size: 5}]}]};
+*  let drive_a = DataArea.from_json(json);
+*/
 
+// from_array
+let drive_a = DataArea.from_array([
+  "A:/", 200, [
+    ["foo", 20, [
+      ["bar", 5],
+      ["baz", 1],
+      ["bag", 3],
+      ["bad", 4],
+      ["baa", 2]]],
+    ["rhubarb", 5],
+    ["1", 30],
+    ["2", 60],
+    ["3", 70],
+    ["4", 15]]]);
 
-function random_colour() {
-  let hex = "0123456789ABCDEF";
-  let out = ["#"];
-  for (var i = 0; i < 3; i++) {
-    out.push(hex[Math.floor(Math.random() * 16.5)])
-  }
-  return out.join("");
-}
+// TODO: wait for stylesheet to finish loading before setting data
+let drive_div = document.querySelector(".from > .drive");
+drive_div.appendChild(drive_a.as_html_wds());
 
-
-class DataArea {  // drive / file / folder
-  constructor(label, size) {
-    this.label = label;
-    this.size = size;
-    this.children = [];
-  }
-
-  // TODO: index children by label
-  // TODO: child grid-column / row (might be irrelevant)
-  // TODO: drive <div> rect area matching drive capacity
-
-  get used_size() {
-    let sizes = this.children.map((c) => c.size);
-    return sizes.reduce((a, b) => a + b, 0);
-  }
-
-  get free_size() {  // unmapped by children
-    return this.size - this.used_size;
-  }
-
-  alloc(label, size) {
-    if (this.free_size >= size) {
-      this.children.push(new DataArea(label, size));
-    } else {
-      console.error(`${size} > ${this.free_size}`)
-    }
-    return this;  // allow chaining
-  }
-
-  free(label) {
-    this.children = this.children.filter((c) => c.label != label);
-  }
-
-  child(label) {
-    for (const c of this.children) {
-      if (c.label == label) {
-        return c;
-      }
-    }
-  }
-
-  xpath(path) {
-    let out = this;
-    for (const label of path.split("/")) {
-      out = out.child(label);
-    }
-    return out;
-  }
-
-  // TODO: tree view
-  // TODO: on click "select" callback
-
-  // WinDirStat visualiser
-  style(depth = 0) {
-    // grid layout
-    let axes = ["row", "column"]
-    let axis = axes[depth % 2]
-    let out = [`grid-template-${axis}s:`];
-    let sizes = this.children.map(({ size }) => size);
-    sizes.push(this.free_size)
-    for (const size of sizes) { out.push(`${size}fr`); }
-    let colour;
-    if (depth > 0) {
-      colour = random_colour();
-    } else {
-      colour = "#DDD";
-    }
-    let background = [
-      `linear-gradient(0deg in oklch, ${colour}, ${colour})`,
-      "linear-gradient(to left,   #999, transparent 1.5rem)",
-      "linear-gradient(to right,  #999, transparent 1.5rem)",
-      "linear-gradient(to top,    #999, transparent 1.5rem)",
-      "linear-gradient(to bottom, #999, transparent 1.5rem)"];
-    return out.join(" ") + ";" + ` background: ${background.join(", ")};`;
-  }
-
-  as_html_wds(depth = 0) {
-    /* WinDirStat blocks */
-    let div = document.createElement("div");
-    // TODO: xpath id
-    div.setAttribute("class", "data-area");
-    div.setAttribute("style", this.style(depth));
-    for (const c of this.children) {
-      div.appendChild(c.as_html_wds(depth + 1));
-    }
-    div.appendChild(document.createElement("div"));  // misc
-    return div;
-  }
-}
-
-
-// TODO: collapsible list tree of drive contents
-// -- and controls to map sub-folders / files
-
-// TODO: update UI based on drive selection
-// TODO: user controls to map drive contents
-
-// TODO: copy from.drive(path) -> to.drive(path)
-
-
-function main() {
-  // test data
-  let drive_a = new DataArea("A:/", 200)
-    .alloc("foo", 20)
-    .alloc("bar", 10);
-  drive_a.child("foo").alloc("rhubarb", 5);
-
-  // TODO: wire up to <select>
-  let drive_div = document.querySelector(".from > .drive");
-  drive_div.appendChild(drive_a.as_html_wds());
-}
+// TODO:
+// -- wire up list of drives to <select>
+// -- collapsible list tree of drive contents
+// --- and controls to map sub-folders / files
+// -- update UI based on drive selection
+// -- user controls to map drive contents
+// -- copy from.drive(path) -> to.drive(path)
+// -- global drive list (top-level DataAreas)
+// -- json from file (Drag & Drop?) / browser storage
+// -- drive <div> rect area matching drive capacity
+// -- mirror console.log etc. to <code id="log"></code>
+// --- console.assert(cond, msg)
+// --- if not cond: log.append(msg)
